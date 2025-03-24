4c4c1c36115f490fb58848d9f97d6149ccefd5bd
Eliot Bendinelli <eliot.bendinelli@protonmail.com>
2025-03-24 22:00:06 +0100

    Init commit

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+export const SITE = {
+  website: "https://bndy.org/", // replace this with your deployed domain
+  author: "Eliot Bendinelli",
+  profile: "https://bndy.org/",
+  desc: "A minimal portfolio and blog for Eliot Bendinelli.",
+  title: "Eliot Bendinelli",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: true,
+    text: "Suggest Changes",
+    url: "https://github.com/satnaing/astro-paper/edit/main/",
+  },
+  dynamicOgImage: true,
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Europe/Berlin", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
---
+++ src/constants.ts
@@ -0,0 +1,95 @@
+import IconMail from "@/assets/icons/IconMail.svg";
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconBrandX from "@/assets/icons/IconBrandX.svg";
+import IconMastodon from "@/assets/icons/IconMastodon.svg";
+import IconBluesky from "@/assets/icons/IconBluesky.svg";
+import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
+import IconFacebook from "@/assets/icons/IconFacebook.svg";
+import IconTelegram from "@/assets/icons/IconTelegram.svg";
+import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import { SITE } from "@/config";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    href: "https://github.com/ebendinelli",
+    linkTitle: ` ${SITE.title} on Github`,
+    icon: IconGitHub,
+  },
+  {
+    name: "Mastodon",
+    href: "https://mamot.fr/@bendineliot",
+    linkTitle: `${SITE.title} on Mastodon`,
+    icon: IconMastodon,
+  },
+  {
+    name: "Bluesky",
+    href: "https://bsky.app/profile/bndy.org",
+    linkTitle: `${SITE.title} on Bluesky`,
+    icon: IconBluesky,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/eliotbendinelli/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    icon: IconLinkedin,
+  },
+  {
+    name: "Mail",
+    href: "mailto:eliot@bndy.org",
+    linkTitle: `Send an email to ${SITE.title}`,
+    icon: IconMail,
+  },
+] as const;
+
+export const SHARE_LINKS = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/?text=",
+    linkTitle: `Share this post via WhatsApp`,
+    icon: IconWhatsapp,
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/sharer.php?u=",
+    linkTitle: `Share this post on Facebook`,
+    icon: IconFacebook,
+  },
+  {
+    name: "X",
+    href: "https://x.com/intent/post?url=",
+    linkTitle: `Share this post on X`,
+    icon: IconBrandX,
+  },
+  {
+    name: "Mastodon",
+    href: "https://mamot.fr/bendineliot",
+    linkTitle: `${SITE.title} on Mastodon`,
+    icon: IconMastodon,
+  },
+  {
+    name: "Bluesky",
+    href: "https://mamot.fr/bendineliot",
+    linkTitle: `${SITE.title} on Bluesky`,
+    icon: IconBluesky,
+  },
+  {
+    name: "Telegram",
+    href: "https://t.me/share/url?url=",
+    linkTitle: `Share this post via Telegram`,
+    icon: IconTelegram,
+  },
+  {
+    name: "Pinterest",
+    href: "https://pinterest.com/pin/create/button/?url=",
+    linkTitle: `Share this post on Pinterest`,
+    icon: IconPinterest,
+  },
+  {
+    name: "Mail",
+    href: "mailto:?subject=See%20this%20post&body=",
+    linkTitle: `Share this post via email`,
+    icon: IconMail,
+  },
+] as const;
